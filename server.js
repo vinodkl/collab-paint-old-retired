2761518ed3c5ea12c6a10c29078349b4d46f8478
vinod <tquester@megatron.(none)>
2013-08-26 23:43:58 -0700

    test case for server added

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-exports.number = function() {
-	return 3;
-}
+"use strict";
+
+var http = require('http');
+var server = http.createServer();
+
+exports.start = function() {	
+	
+	server.on('request', function(req, res) {
+		var body = "<html><head><title></title></head>" +
+					"<body><p>Node server running</p></body></html>";
+		res.end(body);
+	});
+
+	server.listen(8081);
+};
+
+exports.stop = function(callback) {
+	server.close(callback);
+};
--- test/server_test.js
+++ test/server_test.js
@@ -1,6 +1,15 @@
 var server = require("../server.js");
+var http = require('http');
 
-exports.testNothing = function(test) {
-	test.equal(3, server.number(), "failed assertion on server.number");
-	test.done();
-}
+exports.tearDown = function(done) {
+	server.stop(function() {
+		done();
+	});
+};
+
+exports.testHTTPServer = function(test) {
+	server.start();
+	http.get('http://localhost:8081', function(res) {
+		test.done();
+	});
+};
